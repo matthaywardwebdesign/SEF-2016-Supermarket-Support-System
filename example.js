aca637ecf833492368dee18769fa31699b76cd6a
root <root@sef.rmit>
2016-03-31 02:18:19 -0400

    Add example code

---
+++ example.js
@@ -0,0 +1,20 @@
+//items.js
+
+module.exports.add = function(data, callback){
+  r.table("items").insert(data).run(rconn, callback);
+}
+
+//This function is not accessible from any other class
+
+function bob(){
+
+}
+
+//app.js
+
+var items = require("./items.js");
+items.add({id: 100, name: "Pink Lady Apple", price: 10.40}, function(err){
+  
+});
+
+
