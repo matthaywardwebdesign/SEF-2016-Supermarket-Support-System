657f4ce247769d600138484915c260e31a2d9e89
Matthew HAYWARD <matt@matthayward.com.au>
2016-04-20 09:59:20 +1000

    Added ability to delete an item

--- index.js
+++ index.js
@@ -15,22 +15,11 @@ var error = require("./lib/error.js");
 
 db.connect(function(err){
    if (!err){
-      // var item = new Item({
-      //    "id": 133211,
-      //    "name": "Banana Cavendish",
-      //    "ean":4011,
-      //    "price":2.70,
-      //    "isWeighted": true
-      // });
-      // items.saveItem(item, function (err){
-      //    console.log("Item Added");
-      // });
-      items.getItemByEAN(4011, function (err, item){
-         var result = item.removeStock(400);
-         if (result != null){
+      items.deleteItemByID(311211, function (err){
+         console.log(err);
+         items.getAllItems(function (err, result){
             console.log(result);
-         }
-         items.saveItem(item, function(){});
+         });
       });
    }
 });
