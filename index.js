3352b2e4e265854d627eafe12c1fee77b853b4d7
Matthew HAYWARD <matt@matthayward.com.au>
2016-04-20 09:50:55 +1000

    Added ability to manage stock level of items

--- index.js
+++ index.js
@@ -15,11 +15,22 @@ var error = require("./lib/error.js");
 
 db.connect(function(err){
    if (!err){
-      items.getItemByID(311211, function (err, item){
-         console.log(item);
-      });
-      items.getAllItems(function (err, items){
-         console.log(items);
+      // var item = new Item({
+      //    "id": 133211,
+      //    "name": "Banana Cavendish",
+      //    "ean":4011,
+      //    "price":2.70,
+      //    "isWeighted": true
+      // });
+      // items.saveItem(item, function (err){
+      //    console.log("Item Added");
+      // });
+      items.getItemByEAN(4011, function (err, item){
+         var result = item.removeStock(400);
+         if (result != null){
+            console.log(result);
+         }
+         items.saveItem(item, function(){});
       });
    }
 });
