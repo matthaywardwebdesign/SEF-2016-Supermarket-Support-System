ba386260d640434bcecf836f6e8223dd34ae7ad8
Matthew HAYWARD <matt@matthayward.com.au>
2016-04-21 10:15:56 +1000

    Made a few little bug fixes for the transaction code and the transaction tests

--- test/transaction.js
+++ test/transaction.js
@@ -8,7 +8,7 @@ var Transaction = require("../lib/transaction.js");
 var testData = {
    "transactionNo":40,
    "customerNo":55,
-   "date": '2016-1-03 12:55:40',
+   "date": "Thu Apr 21 2016 10:08:05 GMT+1000 (AEST)",
    "gst":2.90,
    "paymentType":"credit",
    "totalBulkDiscount":0,
@@ -23,14 +23,14 @@ tests.before = function(done){
 }
 
 tests.beforeEach = function(done){
-   var transaction = new transaction(testData);
+   var transaction = new Transaction(testData);
    transactions.saveTransaction(transaction, function(){
       done();
    });
 }
 
 tests.after = function(done){
-   transactions.deleteTransactionByID(testData.transactionNo, function(){
+   transactions.deleteTransactionByNumber(testData.transactionNo, function(){
       var conn = db.getConnection();
       conn.close();
       done();
@@ -44,14 +44,14 @@ tests.afterEach = function(done){
 module.exports.testGetTransactionNo = function(test){
    var transaction = new Transaction(testData);
    test.expect(1);
-   test.equal(transaction.getTransactionNo(), testData.transactionNo, "Failed to get transaction ID");
+   test.equal(transaction.getTransactionNo(), testData.transactionNo, "Failed to get transaction number");
    test.done();
 }
 
 module.exports.testGetCustomerNo = function(test){
    var transaction = new Transaction(testData);
    test.expect(1);
-   test.equal(transaction.getCustomerNo(), testData.customerNo, "Failed to get cusomter number");
+   test.equal(transaction.getCustomerNo(), testData.customerNo, "Failed to get customer number");
    test.done();
 }
 
@@ -62,37 +62,39 @@ module.exports.testGetDate = function(test){
    test.done();
 }
 
-   module.exports.testGetGst = function(test){
+module.exports.testGetGST = function(test){
    var transaction = new Transaction(testData);
    test.expect(1);
-   test.equal(transaction.getGst(), testData.gst, "Failed to get item Gst");
+   test.equal(transaction.getGST(), testData.gst, "Failed to get item GST");
    test.done();
 }
 
-   module.exports.testGetPaymentType = function(test){
+module.exports.testGetPaymentType = function(test){
    var transaction = new Transaction(testData);
    test.expect(1);
-   test.equal(transaction.getPaymentType(), testData.paymentType, "Failed to get item Payment Type");
+   test.equal(transaction.getPaymentType(), testData.paymentType, "Failed to get transaction payment type");
    test.done();
 }
 
-   module.exports.testGetBulkDiscount = function(test){
+module.exports.testGetTotalBulkDiscount = function(test){
    var transaction = new Transaction(testData);
    test.expect(1);
-   test.equal(transaction.getBulkDiscount(), testData.totalBulkDiscount, "Failed to get item Bulk Discount");
+   test.equal(transaction.getTotalBulkDiscount(), testData.totalBulkDiscount, "Failed to get transaction bulk discount");
    test.done();
 }
 
-   module.exports.testGetNoTransactionItems = function(test){
+module.exports.testGetNoTransactionItems = function(test){
    var transaction = new Transaction(testData);
    test.expect(1);
-   test.equal(transaction.getNoTransactionItems(), testData.noTransactionItems, "Failed to get item No Transaction Items");
+   test.equal(transaction.getNoTransactionItems(), testData.noTransactionItems, "Failed to get number of transaction items");
    test.done();
 }
 
-   module.exports.testGetTotal = function(test){
+module.exports.testGetTotal = function(test){
    var transaction = new Transaction(testData);
    test.expect(1);
-   test.equal(transaction.getTotal(), testData.total, "Failed to get item Total");
+   test.equal(transaction.getTotal(), testData.total, "Failed to get transaction total");
    test.done();
 }
+
+tests.load(module.exports);
