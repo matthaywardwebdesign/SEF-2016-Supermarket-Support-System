ff9b19f1308f152058b7a112325c45c41caca898
Matthew HAYWARD <matt@matthayward.com.au>
2016-04-21 10:38:00 +1000

    Added the ability to add an item to a transaction, we do need a TransactionLine module though

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var items = require("./lib/items.js");
 var Item = require("./lib/item.js");
 var r = db.getLibrary();
 var error = require("./lib/error.js");
+var transactions = require("./lib/transactions.js");
+var Transaction = require("./lib/transaction.js");
 
 //Connect to the database
 //note - RMIT blocks the port used by RethinkDB, run this code on the server
@@ -15,11 +17,25 @@ var error = require("./lib/error.js");
 
 db.connect(function(err){
    if (!err){
-      items.deleteItemByID(311211, function (err){
-         console.log(err);
-         items.getAllItems(function (err, result){
-            console.log(result);
-         });
+      var item = new Item({
+         "ean": 4011 ,
+         "id": 133211 ,
+         "isWeighted": true ,
+         "name":  "Banana Cavendish" ,
+         "price": 2.7 ,
+         "soh": 0
       });
+
+      items.saveItem(item, function (err){
+         var t = new Transaction({
+            "transactionNo":40,
+            "customerNo":55,
+            "date": new Date().toString(),
+         });
+         t.addItem(item);
+         transactions.saveTransaction(t, function (err){
+            console.log("Transaction saved");
+         });
+      })
    }
 });
