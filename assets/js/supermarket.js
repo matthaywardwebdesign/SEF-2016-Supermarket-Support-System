fb83d76fdbda5e75d66b2fdaae9144b5fdd984ea
Matt Hayward <matt@matthayward.com.au>
2016-04-28 21:01:05 +1000

    Fixed up the bug that was causing the test cases to fail as well as the application itself, unit test cases ftw

--- assets/js/supermarket.js
+++ assets/js/supermarket.js
@@ -1,5 +1,6 @@
 var Supermarket = {};
 (function(){
+
    Supermarket.createTransaction = function(callback){
       API.post("transaction", {"customerNo":1}, function(data){
          callback(data.id);
@@ -15,4 +16,5 @@ var Supermarket = {};
    Supermarket.loadPanel = function(panel){
       $("#panel-holder").load("/panel/" + panel);
    }
+
 })();
