d07ac0cda121a0f9155a426e67ca6d4b53524ce6
sirtoxic <leerussell1991@gmail.com>
2016-05-10 14:38:24 +1000

    added panel for item lookup

--- controller/panel-lookup-item.js
+++ controller/panel-lookup-item.js
@@ -3,9 +3,32 @@ var item = require("../lib/item.js");
 
 module.exports.run = function(req, res, template){
    var data = {};
-   items.getAllItems(function (err, items){
+   data.items = [];
+   var type = req.query.type;
+   var search = req.query.search;
+
+   if(type == "id"){
+   	items.getItemByID(function (err, items){
       if (err){res.end(err);return;}
-      data.items = items;
+      data.items = [item];
       res.end(template(data));
-   });
-}
+   	});
+   }
+   if(type == "ean"){
+   	items.getItemByEAN(function(err, items){
+   		if (err){res.end(err);return;}
+         data.items = [item];
+         res.end(template(data));
+      });
+   }
+   if(type == "name"){
+   	items.getItemByName(function(err, items){
+      if (err){res.end(err);return;}
+         data.items = items;
+      });
+   }
+   if(type == null){
+   	  res.end();
+        return null;
+   	}
+   }
