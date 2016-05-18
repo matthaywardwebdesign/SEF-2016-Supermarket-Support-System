22d251cba71be83169d329543eb88cd25cffb9c7
Matt Hayward <matt@matthayward.com.au>
2016-05-19 00:07:31 +1000

    Added pay now panel

---
+++ controller/panel-pay-now.js
@@ -0,0 +1,20 @@
+var transactions = require("../lib/transactions.js");
+var transaction = require("../lib/transaction.js");
+var error = require("../lib/error.js");
+
+module.exports.run = function(req, res, template){
+   var data = {};
+   var transactionID = parseInt(req.cookies.currentTransaction);
+   transactions.getTransactionByID(transactionID, function (err, transaction){
+      if (err == error.ERROR_TRANSACTION_NO_EXIST){
+         res.clearCookie("currentTransaction");
+         res.send("<script>document.location.href='/';</script>");
+      }
+      if (err){res.end(err);return;}
+         transaction.calculate(function (){
+            console.log(transaction);
+            data.transaction = transaction;
+            res.end(template(data));
+         });
+   });
+}
