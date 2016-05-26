3d0db5d7bb370044757733f99b1637fc1b4c4c4e
Matt Hayward <matt@matthayward.com.au>
2016-05-26 19:58:48 +1000

    Working on orders

--- controller/api-transaction-delete.js
+++ controller/api-transaction-delete.js
@@ -3,7 +3,7 @@ module.exports.run = function(req, res){
    var id = parseInt(req.params.id);
    transactions.getTransactionByID(id, function (err, transaction){
       if (err){res.send(err);return;}
-      transaction.setStatus("cancelled");
+      transaction.cancel();
       transactions.saveTransaction(transaction, function(){
          res.send({"success": true});
       });
